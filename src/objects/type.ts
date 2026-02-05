d0545582290bcc1085eb795bf16b8ca067feb3fa
René Rivas <reinaldorrivas@gmail.com>
2026-02-05 19:06:17 -0400

    fh-ts-guide: Custom TS types intro

---
+++ src/objects/type.ts
@@ -0,0 +1,29 @@
+type SuperHeroe = {
+  name: string;
+  age: number;
+  powers: string[];
+  getName?: () => string;
+};
+
+export const typeTs: () => void = (): void => {
+  const flash: SuperHeroe = {
+    name: "Barry Allen",
+    age: 24,
+    powers: ["Súper Velocidad", "Time travel"],
+    getName() {
+      return this.name;
+    },
+  };
+
+  console.log(flash.getName?.());
+
+  const superman: SuperHeroe = {
+    name: "Clark Kent",
+    age: 60,
+    powers: ["Súper Fuerza"],
+  };
+
+  console.log({ superman });
+
+    console.log("--- END CUSTOM TS TYPES FILE ---");
+};
